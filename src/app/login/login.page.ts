c9e80fedd566f50766bac1f50d848a13c035948d
Manueljogu <ma.guara@duocuc.cl>
2024-10-27 14:37:24 -0300

    Creacion de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  user = {
+    username: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  login() {
+    if (this.user.username.length >= 3 && this.user.username.length <= 20 && this.user.password.length === 4) {
+      this.router.navigate(['/home'], { state: { user: this.user } });
+    } else {
+      alert('Por favor, ingrese un usuario y una contraseña válidos.');
+    }
   }
-
 }
